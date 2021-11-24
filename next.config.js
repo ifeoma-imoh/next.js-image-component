6443745d25eccd3569385175a78e2bf04d368fb5
Ifeoma Imoh <imoh_ifeoma@yahoo.co.uk>
2021-11-24 18:00:51 +0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  target: "serverless",
+  images: {
+    domains: ["res.cloudinary.com"],
+    // loader: "cloudinary",
+    // path: "https://res.cloudinary.com/ifeomaimoh/image/upload/",
+  },
+};
